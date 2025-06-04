7c5e937d675a9ca49d36ed068e34acbdcdd27553
Vishal853 <vishal@cloudframe.com>
2025-06-04 10:39:34 +0530

    Implemented web for order screens

--- App.tsx
+++ App.tsx
@@ -4,12 +4,12 @@ import { useFonts, Inter_400Regular, Inter_500Medium, Inter_600SemiBold, Inter_7
 
 // import { LanguageProvider } from './context/LanguageContext';
 import './global.css';
-import { Text, View } from 'react-native';
+import { Platform, Text, View } from 'react-native';
 import LanguageSelectionScreen from 'screens/LanguageSelectionScreen';
 import { useEffect } from 'react';
 
 export default function App() {
-  
+
   const [fontsLoaded] = useFonts({
     Inter_400Regular,
     Inter_500Medium,
@@ -26,12 +26,27 @@ export default function App() {
   if (!fontsLoaded) return null;
 
   return (
-    <View className="flex-1 bg-[#5b7d57] items-center justify-center">
-      <View className="flex-1 w-full max-w-[412px] bg-[#380b0b]">
-        <NavigationContainer>
-          <RootNavigator />
-        </NavigationContainer>
-      </View>
-    </View>
+    <>
+      {
+        Platform.OS === 'web' ? (
+          <View className="flex-1 bg-white items-center justify-center ">
+            <View className="flex-1 w-full max-w-[700px] bg-white m-10" >
+              <NavigationContainer>
+                <RootNavigator />
+              </NavigationContainer>
+            </View>
+          </View>
+        ) : (
+          <View className="flex-1 bg-[#5b7d57] items-center justify-center">
+            <View className="flex-1 w-full max-w-[412px] bg-[#380b0b]">
+              <NavigationContainer>
+                <RootNavigator />
+              </NavigationContainer>
+            </View>
+          </View>
+        )
+      }
+    </>
+
   );
 }
